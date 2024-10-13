84c58ae406c4deee752184f3462ead812af36ff7
Danil Myshakin <Tyuiu.MyshakinD@outlook.com>
2024-10-13 05:32:22 +0500

    Task3

---
+++ Tyuiu.MyshakinD.Sprint2.Task3.V27.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using Tyuiu.MyshakinD.Sprint2.Task3.V27.Lib;
+
+namespace Tyuiu.MyshakinD.Sprint2.Task3.V27.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = -5;
+            var res = Math.Round(ds.Calculate(x), 3);
+            double wait = 1.082;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MyshakinD.Sprint2.Task3.V27/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.MyshakinD.Sprint2.Task3.V27.Lib;
+
+namespace Tyuiu.MyshakinD.Sprint2.Task3.V27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            
+            Console.Title = "Спринт #2 | Выполнил: Мышакин Д. | ПКТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Вложенные операторы if - else                                     *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Выполнил: Мышакин Данил              | ПКТб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y с    *");
+            Console.WriteLine("* использованием вложенных оператор if-else, где пользователь вводит      *");
+            Console.WriteLine("* значение переменной X с клавиатуры. Округлить полученное значение до    *");
+            Console.WriteLine("* трех знаков после запятой;                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите X:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            var y = Math.Round(ds.Calculate(x), 3);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(y);
+            Console.ReadKey();
+
+        }
+    }
+}
